bd1c8df257d0219c3bfab287ac109651bf81bdb4
Tom Lambert <tom.lambert.13@live.com>
2016-05-29 23:00:49 +0200

    Added "Why implementing an interface instead of inherit a class?"

---
+++ Why implementing an interface instead of inherit a class/Why implementing an interface instead of inherit a class/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace WIAIIOIAC
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // ReSharper disable once JoinDeclarationAndInitializer
+            IBase a;
+
+            a = new InheritedSubClass();
+            a.Output();
+
+            a = new ImplementingdSubClass();
+            a.Output();
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IBase
+    {
+        void Output();
+    }
+
+    public class BaseClass : IBase
+    {
+        public void Output()
+        {
+            Console.WriteLine("BaseClass");
+        }
+    }
+
+    public class InheritedSubClass : BaseClass
+    {
+        public new void Output()
+        {
+            Console.WriteLine("SubClass");
+        }
+    }
+
+    public class ImplementingdSubClass : IBase
+    {
+        public void Output()
+        {
+            Console.WriteLine("SubClass");
+        }
+    }
+}
