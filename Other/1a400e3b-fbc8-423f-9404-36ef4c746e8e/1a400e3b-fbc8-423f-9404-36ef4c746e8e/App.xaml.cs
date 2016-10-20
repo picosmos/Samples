3ed07a8592cc047621f75980280c69a03c73afb7
Tom Lambert <tom.lambert.13@live.com>
2016-10-20 19:26:42 +0200

    Added Other/1a400e3b-fbc8-423f-9404-36ef4c746e8e

---
+++ Other/1a400e3b-fbc8-423f-9404-36ef4c746e8e/1a400e3b-fbc8-423f-9404-36ef4c746e8e/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _1a400e3b_fbc8_423f_9404_36ef4c746e8e
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Other/1a400e3b-fbc8-423f-9404-36ef4c746e8e/1a400e3b-fbc8-423f-9404-36ef4c746e8e/HomeViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using _1a400e3b_fbc8_423f_9404_36ef4c746e8e.Annotations;
+
+namespace _1a400e3b_fbc8_423f_9404_36ef4c746e8e
+{
+    public class HomeViewModel:INotifyPropertyChanged
+    {
+        public ObservableCollection<Node> nodes;
+        public ObservableCollection<Node> Nodes
+        {
+            get { return this.nodes; }
+            set
+            {
+                if (value != this.Nodes)
+                {
+                    this.nodes = value;
+                    this.NotifyOfPropertyChange();
+                }
+            }
+        }
+
+        public HomeViewModel()
+        {
+            this.Nodes = new ObservableCollection<Node>();
+            var node = new Node();
+            node.Location = "Raum1";
+            this.Nodes.Add(node);
+
+            node = new Node();
+            node.Location = "Raum2";
+            this.Nodes.Add(node);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void NotifyOfPropertyChange([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class Node
+    {
+        public string Location { get; set; }
+    }
+}
