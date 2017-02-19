f57e4f47f25e569112b04072f75bd5e3d477ddf2
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-02-19 04:19:19 +0100

    Changed existing drawing tech

--- Snake/Snake.Windows.Forms/GameForm.cs
+++ Snake/Snake.Windows.Forms/GameForm.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using Koopakiller.Apps.Snake.Portable;
 
 namespace Koopakiller.Apps.Snake.Windows.Forms
 {
-    public partial class GameForm : Form
+    public partial class GameForm : Form, IGameDisplay
     {
         public GameForm()
         {
@@ -25,12 +26,43 @@ namespace Koopakiller.Apps.Snake.Windows.Forms
             e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(255, 32, 32, 32)), 0, 0, this.Width, this.Height);
             for (var col = 0; col < this.widthInBlocks; ++col)
             {
-                e.Graphics.DrawLine(Pens.Teal, col * this.pixelPerBlock, 0, col * this.pixelPerBlock, this.Height);
+                for (var row = 0; row < this.heightInBlocks; ++row)
+                {
+                    this.DrawEmptyBlock(e.Graphics, col, row);
+                }
             }
-            for (var row = 0; row < this.heightInBlocks; ++row)
-            {
-                e.Graphics.DrawLine(Pens.Teal, 0, row * this.pixelPerBlock, this.Width, row * this.pixelPerBlock);
-            }
+        }
+
+        void DrawEmptyBlock(Graphics g, Int32 col, Int32 row)
+        {
+            g.FillRectangle(new SolidBrush(Color.FromArgb(255, 32, 32, 32)), col * this.pixelPerBlock, row * this.pixelPerBlock, (col + 1) * this.pixelPerBlock, (row + 1) * this.pixelPerBlock);
+            g.DrawLine(Pens.Teal, col * this.pixelPerBlock, row * this.pixelPerBlock, (col + 1) * this.pixelPerBlock, row * this.pixelPerBlock);
+            g.DrawLine(Pens.Teal, col * this.pixelPerBlock, row * this.pixelPerBlock, col * this.pixelPerBlock, (row + 1) * this.pixelPerBlock);
+        }
+
+        public void DrawSnake(Portable.Snake snake)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSnake(Portable.Snake snake)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawItem(Position position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetPosition(Position position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
         }
     }
 }
