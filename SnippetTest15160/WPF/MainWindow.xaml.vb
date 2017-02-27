ddf49136f2f660a611fe10d7c141a61f19d84a69
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-02-27 22:33:58 +0100

    Snippet Test 15160 https://dotnet-snippets.de/snippet/objekte-z-b-formen-ohne-border-mit-der-maus-verschieben/15160

---
+++ SnippetTest15160/WPF/MainWindow.xaml.vb
@@ -0,0 +1,32 @@
+﻿Class MainWindow
+
+
+
+    Private Sub UIElement_OnMouseDown(sender As Object, e As MouseButtonEventArgs)
+        Dim pos = e.GetPosition(DirectCast(sender, IInputElement))
+        ObjectMouseDown(pos.X, pos.Y)
+    End Sub
+
+    Private Sub UIElement_OnMouseMove(sender As Object, e As MouseEventArgs)
+        if (e.LeftButton = MouseButtonState.Pressed)
+            Dim pos = e.GetPosition(DirectCast(sender, IInputElement))
+            ObjectMove(Me, pos.X, pos.Y)
+        End If
+    End Sub
+
+End Class
+
+
+Module Module_Move
+    Dim X1, Y1 As Integer
+
+    Sub ObjectMouseDown(ByVal X As Integer, ByVal Y As Integer)
+        X1 = X
+        Y1 = Y
+    End Sub
+
+    Sub ObjectMove(ByVal Obj As Object, ByVal X2 As Integer, ByVal Y2 As Integer)
+        Obj.Left = Obj.Left + (X2 - X1)
+        Obj.Top = Obj.Top + (Y2 - Y1)
+    End Sub
+End Module
