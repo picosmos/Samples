df5d98d1c3a65a331e8982600907f2b0ffcd70d7
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-26 21:31:48 +0200

    VariableSwap

---
+++ VariableSwap/VariableSwap/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VariableSwap
+{
+    internal class Program
+    {
+private static void Main()
+{
+    var a = 1;
+    var b = 2;
+
+    Console.WriteLine($"a={a}, b={b}");
+
+    // C# 6.0 and prior
+    GenericSwap(ref a, ref b);
+
+    Console.WriteLine($"a={a}, b={b}");
+
+    // C# 7.0 and later
+    (a, b) = (b, a);
+
+    Console.WriteLine($"a={a}, b={b}");
+
+    Console.ReadKey();
+}
+
+private static void GenericSwap<T>(ref T a, ref T b)
+{
+    var tmp = a;
+    a = b;
+    b = tmp;
+}
+    }
+}
