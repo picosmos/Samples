743fdf2e110b3491614ad61d78942d83267c78a9
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-26 21:32:06 +0200

    Added IrrationalNumberType

---
+++ IrrationalNumberType/IrrationalNumberType/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IrrationalNumberType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IArethmetic
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface IIrrationalNumber
+    {
+        IIrrationalNumber Add(IIrrationalNumber a, IIrrationalNumber b);
+    }
+
+    public struct Pi : IIrrationalNumber
+    {
+        
+    }
+}
